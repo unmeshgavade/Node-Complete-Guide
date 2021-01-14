e33921ce56e3fbe1a41b431731a6ef64459ec3b1
Unmesh Gavade <unmeshgavade@gmail.com>
2021-01-14 11:27:17 +0530

    intalled express

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-const http = require('http')
+// const http = require('http');
 
-const routes = require('./routes')
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('In the middleware')
+    next(); // Allows the code to go to next middleware
+})
+
+app.use((req, res, next) => {
+    console.log('In the middleware')
+    // ...
+})
 
 
-console.log(routes.someText)
-const server = http.createServer(routes.handler)
+// const server = http.createServer(app);
 
-server.listen(3000)
+// server.listen(3000);
+
+app.listen(3000)
