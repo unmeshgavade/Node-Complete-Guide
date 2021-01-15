a14bf319837c5c536c5daa3d1655947d28ee28de
Unmesh Gavade <unmeshgavade@gmail.com>
2021-01-15 10:06:19 +0530

    views and dynamic html

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
     console.log('In the middleware')
-    // ...
-    res.send('<form action="/add-product" method="POST"><input type="text" name="message"><button type="submit">Add</button></form>')
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
 });
 
 router.post('/add-product', (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path')
 
 const router = express.Router();
 
-router.use('/', (req, res, next) => {
+router.get('/', (req, res, next) => {
     console.log('In the middleware')
-    res.send('<h1>Hello from Express</h1>')
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'))
 })
 
 module.exports = router;
