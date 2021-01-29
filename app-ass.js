e370f1ee12cd500e2395414df85261a8824e6f39
Unmesh Gavade <unmeshgavade@gmail.com>
2021-01-29 13:16:08 +0530

    mysql

--- app-ass.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-
-const routes = require('./routes-ass')
-
-const server = http.createServer(routes.handler)
-
-server.listen(3000)
--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 const app = express();
+
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
